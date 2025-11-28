43f8f5b24949270a5990f9ded45aa4f02595a9a3
fj0r <fj0rd@qq.com>
2025-11-28 17:51:20 +0800

    brand

--- crates/chat/src/libs/logic/crm.rs
+++ crates/chat/src/libs/logic/crm.rs
@@ -2,6 +2,8 @@ use super::super::handler::{ArcShared, ChatMessage, Envelope, Sender};
 use anyhow::Result;
 use std::fmt::Debug;
 
+const _BRAND: &str = "kairos开若";
+
 pub async fn crm<T: Debug>(e: ChatMessage<T>, s: ArcShared, x: Sender<T>) -> Result<()> {
     println!("crm => {:?}", e);
     Ok(())
